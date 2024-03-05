8cbef2050040a2dcfd2aa31fba57f0f4bd8d7fb1
mehannioui <118213854+mehannioui@users.noreply.github.com>
2024-03-05 08:54:20 -0500

    models and routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -10,10 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api/test', async (req: Request, res: Response) => {
-  res.json({ message: 'hello from express endpoint!' });
-});
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
-app.listen(7001, () => {
-  console.log('Server is running on port 7000');
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
