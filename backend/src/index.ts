18133d986c922f06dbc6ef83cc30123eaacf62cb
mehannioui <118213854+mehannioui@users.noreply.github.com>
2024-03-05 00:29:06 -0500

    create header, hero and footer components

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
@@ -12,5 +15,5 @@ app.get('/api/test', async (req: Request, res: Response) => {
 });
 
 app.listen(7001, () => {
-  console.log('Server is running on port 7000+188');
+  console.log('Server is running on port 7000');
 });
